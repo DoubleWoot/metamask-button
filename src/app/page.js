73d9a9331c9823b5881cd488b0484cc165f3ca33
DoubleWoot <202110423@feualabang.edu.ph>
2024-12-17 00:46:15 +0800

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,70 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [error, setError] = useState(null);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  // function for MetaMask connection
+  const connectAccount = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        // account request
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const account = accounts[0];
+        setAccount(account);
+
+        // balance fetch and display
+        const balanceHold = await window.ethereum.request({
+          method: "eth_getBalance",
+          params: [account, "latest"],
+        });
+
+        // converting balance into float with a set precision to 4
+        const balanceEth = parseFloat(parseInt(balanceHold, 16) / 1e18).toFixed(
+          4
+        );
+        setBalance(balanceEth);
+      } catch (err) {
+        setError(err.message);
+      }
+    } else {
+      setError("Please install MetaMask"); // will show up if the MetaMask extension is not installed
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <button
+          className={styles.meta_button}
+          type="button"
+          onClick={connectAccount}
+        >
+          Connect With MetaMask
+          <Image
+            className={styles.meta_img}
+            src="/metamask-icon.png"
+            alt="MetaMask icon"
+            width={24}
+            height={24}
+          />
+        </button>
+        <div className={styles.balance}>
+          <p>
+            <strong>Your Total Balance: </strong> {balance}
+          </p>
+          <p>
+            <strong>Account Address: </strong> {account}
+          </p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
